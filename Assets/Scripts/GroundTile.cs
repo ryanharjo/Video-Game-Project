1f85cf10141bbc77b2c7869cef23ec6148623ac7
Ryan Harjo <ryanharjo01@gmail.com>
2026-01-12 08:46:58 -0600

    commit

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    GroundSpawner groundSpawner;
+
+    private void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+    }
+
+    private void OnTriggerExit (Collider other)
+    {
+        groundSpawner.SpawnTile();
+        Destroy(gameObject, 2);
+    }
+}
