2221766db5655738c669ad1b2530fcb2acc15ae7
Ryan Harjo <ryanharjo01@gmail.com>
2026-01-19 18:20:17 -0600

    commit

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Scene")]
+    public string gameSceneName = "new scene";
+
+    [Header("Panels")]
+    public GameObject mainPanel;
+    public GameObject creditsPanel;
+    public GameObject settingsPanel;
+
+    // ===== BUTTON FUNCTIONS =====
+
+    public void PlayGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(gameSceneName);
+    }
+
+    public void OpenCredits()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+    }
+
+    public void OpenSettings()
+    {
+        mainPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void Back()
+    {
+        ShowMain();
+    }
+
+    // ===== HELPERS =====
+    void ShowMain()
+    {
+        mainPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        settingsPanel.SetActive(false);
+    }
+}
