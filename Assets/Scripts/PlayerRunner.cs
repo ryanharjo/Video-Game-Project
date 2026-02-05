dd0fdbff96e9aec247bec50c8af41d4f6607c14b
Ryan Harjo <ryanharjo01@gmail.com>
2026-02-05 15:09:52 -0600

    commit

--- Assets/Scripts/PlayerRunner.cs
+++ Assets/Scripts/PlayerRunner.cs
@@ -1,102 +1,147 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 public class PlayerRunner : MonoBehaviour
 {
-    // Movement input
-    private float horizontal;
-    private float vertical;
+    [Header("Movement")]
+    public float forwardSpeed = 10f;
+    public float laneDistance = 3f;
+    public float laneSwitchSpeed = 15f;
 
-    // Jump variables
-    private float jumpTime = 1.0f;
-    private float jumpTimer;
-    private bool hasJumped = false;
-    private bool canJump = true;
+    [Header("Jump")]
+    public float jumpForce = 7f;
+    public float gravity = -25f;
 
-    // Optional speed property (for powerups)
-    public float PlayerSpeed { get; set; } = 10f;
-
-    // Physics
-    public float gravity = -20f;
-    public float jumpForce = 6f;
+    [Header("Slide")]
+    public float slideTime = 0.8f;
+    public float slideHeightMultiplier = 0.5f;
 
     private CharacterController controller;
-    private Animator anim;
+    private Animator animator;
+    private PlayerInput playerInput;
 
-    private float verticalVelocity;
-    private bool groundedPlayer;
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction slideAction;
+    private InputAction flipAction;
 
-    void Start()
+    private Vector3 direction;
+    private int currentLane = 1;
+    private bool canSwitchLane = true;
+    private bool isSliding = false;
+
+    void Awake()
     {
         controller = GetComponent<CharacterController>();
-        anim = GetComponentInChildren<Animator>();
+        animator = GetComponentInChildren<Animator>();
+        playerInput = GetComponent<PlayerInput>();
 
-        // Initialize jump timer
-        jumpTimer = 0.0f;
+        moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+        slideAction = playerInput.actions["Slide"];
+        flipAction = playerInput.actions["Flip"];
     }
 
-    // New Input System - Move
-    public void Move(InputAction.CallbackContext context)
+    void OnEnable()
     {
-        horizontal = context.ReadValue<Vector2>().x;
-        vertical = context.ReadValue<Vector2>().y;
+        moveAction.Enable();
+        jumpAction.Enable();
+        slideAction.Enable();
+        flipAction.Enable();
     }
 
-    // New Input System - Jump
-    public void Jump(InputAction.CallbackContext context)
+    void OnDisable()
     {
-        hasJumped = context.ReadValueAsButton();
+        moveAction.Disable();
+        jumpAction.Disable();
+        slideAction.Disable();
+        flipAction.Disable();
     }
 
     void Update()
     {
-        groundedPlayer = controller.isGrounded;
+        HandleLaneInput();
 
-        // Forward endless runner movement + horizontal input
-        Vector3 move = new Vector3(horizontal, 0, PlayerSpeed);
+        float targetX = (currentLane - 1) * laneDistance;
+        float newX = Mathf.Lerp(transform.position.x, targetX, laneSwitchSpeed * Time.deltaTime);
+        float xDelta = newX - transform.position.x;
 
-        // Stick to ground
-        if (groundedPlayer && verticalVelocity < 0)
+        if (controller.isGrounded)
         {
-            verticalVelocity = -2f;
-        }
+            if (direction.y < 0) direction.y = -2f;
 
-        // Jump logic (replaces Input.GetButtonDown)
-        if (hasJumped && canJump && groundedPlayer)
-        {
-            verticalVelocity = jumpForce;
-            jumpTimer = jumpTime;
-            canJump = false;
-            groundedPlayer = false;
-
-            anim.SetTrigger("Jump");
-        }
-
-        // Jump cooldown timer
-        if (canJump == false)
-        {
-            jumpTimer -= Time.deltaTime;
-        }
-
-        if (jumpTimer <= 0)
-        {
-            jumpTimer = 0;
-            canJump = true;
+            if (jumpAction.triggered && !isSliding)
+            {
+                direction.y = jumpForce;
+                animator.SetBool("Jump", true);
+            }
+            else
+            {
+                animator.SetBool("Jump", false);
+            }
         }
         else
         {
-            canJump = false;
+            direction.y += gravity * Time.deltaTime;
         }
 
-        // Gravity
-        verticalVelocity += gravity * Time.deltaTime;
-        move.y = verticalVelocity;
+        // Slide
+        if (slideAction.triggered && controller.isGrounded && !isSliding)
+            StartCoroutine(SlideRoutine());
 
-        // Apply movement
-        controller.Move(move * Time.deltaTime);
+        // Flip
+        if (flipAction.triggered && controller.isGrounded && !isSliding)
+            animator.SetTrigger("Flip");
 
-        // Animator
-        anim.SetBool("IsGrounded", groundedPlayer);
-        anim.SetFloat("Speed", PlayerSpeed);
+        direction.x = xDelta;
+        direction.z = forwardSpeed;
+
+        controller.Move(direction * Time.deltaTime);
+
+        animator.SetFloat("Speed", 1f);
+        animator.SetBool("IsGrounded", controller.isGrounded);
+    }
+
+    void HandleLaneInput()
+    {
+        float horizontal = moveAction.ReadValue<Vector2>().x;
+
+        if (canSwitchLane)
+        {
+            if (horizontal > 0.5f && currentLane < 2)
+            {
+                currentLane++;
+                canSwitchLane = false;
+            }
+            else if (horizontal < -0.5f && currentLane > 0)
+            {
+                currentLane--;
+                canSwitchLane = false;
+            }
+        }
+
+        if (Mathf.Abs(horizontal) < 0.1f)
+            canSwitchLane = true;
+    }
+
+    IEnumerator SlideRoutine()
+    {
+        isSliding = true;
+        animator.SetBool("Slide", true);
+
+        float originalHeight = controller.height;
+        Vector3 originalCenter = controller.center;
+
+        controller.height = originalHeight * slideHeightMultiplier;
+        controller.center = originalCenter * slideHeightMultiplier;
+
+        yield return new WaitForSeconds(slideTime);
+
+        controller.height = originalHeight;
+        controller.center = originalCenter;
+
+        animator.SetBool("Slide", false);
+        isSliding = false;
     }
 }
