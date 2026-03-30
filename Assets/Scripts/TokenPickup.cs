02fca97ad0b6313a39de0f187ded13ba4b71827b
Ryan Harjo <ryanharjo01@gmail.com>
2026-03-30 15:15:33 -0500

    commit

--- Assets/Scripts/TokenPickup.cs
+++ Assets/Scripts/TokenPickup.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class TokenPickup : MonoBehaviour
 {
     public AudioClip collectSound;
+    public int tokenValue = 10;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            // Play sound
             AudioSource audio = Camera.main.GetComponent<AudioSource>();
-
             audio.pitch = Random.Range(0.9f, 1.1f);
             audio.PlayOneShot(collectSound);
 
+            // Add tokens
+            GameManager.Instance.AddToken(tokenValue);
+
+            // Destroy token
             Destroy(gameObject);
         }
     }
