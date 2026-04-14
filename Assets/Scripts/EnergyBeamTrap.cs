f3fead8f5fe5516d3c84ad97c193945875e25fc7
Ryan Harjo <ryanharjo01@gmail.com>
2026-04-14 15:08:53 -0500

    commit

--- Assets/Scripts/EnergyBeamTrap.cs
+++ Assets/Scripts/EnergyBeamTrap.cs
@@ -6,8 +6,25 @@ public class EnergyBeamTrap : MonoBehaviour
     {
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Player hit → Game Over");
+            KillPlayer();
+        }
+    }
 
+    private void OnTriggerStay(Collider other)
+    {
+        // This makes sure the player STILL dies
+        // if the beam turns ON while they are inside it
+        if (other.CompareTag("Player"))
+        {
+            KillPlayer();
+        }
+    }
+
+    void KillPlayer()
+    {
+        if (GameManager.Instance.currentState != GameManager.GameState.GameOver)
+        {
+            Debug.Log("⚡ Player killed by energy beam");
             GameManager.Instance.GameOver();
         }
     }
