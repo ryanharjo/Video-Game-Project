8569698071af8c75e3453358b179a1b17656a74b
Ryan Harjo <ryanharjo01@gmail.com>
2026-04-18 20:58:11 -0500

    commit

--- Assets/Scripts/PlayerRunner.cs
+++ Assets/Scripts/PlayerRunner.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 
 public class PlayerRunner : MonoBehaviour
@@ -11,20 +12,22 @@ public class PlayerRunner : MonoBehaviour
     private bool isFinished = false;
 
     [Header("Jump Settings")]
-    public float jumpForce = 5.5f;   // Lower jump
-    public float gravity = -32f;     // Stronger gravity
-    public float fallMultiplier = 1.5f; // Faster falling
+    public float jumpForce = 5.5f;
+    public float gravity = -32f;
+    public float fallMultiplier = 1.5f;
+
+    [Header("UI & Finish Settings")]
+    public GameObject levelCompletePanel;
+    public float uiDelay = 2.0f;
 
     private CharacterController controller;
     private Animator animator;
-
     private PlayerInput playerInput;
     private InputAction moveAction;
     private InputAction jumpAction;
 
     private Vector2 mobileMoveInput;
     private bool mobileJumpInput;
-
     private Vector3 velocity;
     private int currentLane = 1;
     private bool canSwitchLane = true;
@@ -33,10 +36,13 @@ public class PlayerRunner : MonoBehaviour
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponentInChildren<Animator>();
-
         playerInput = GetComponent<PlayerInput>();
+
         moveAction = playerInput.actions["Move"];
         jumpAction = playerInput.actions["Jump"];
+
+        if (levelCompletePanel != null)
+            levelCompletePanel.SetActive(false);
     }
 
     void OnEnable()
@@ -62,81 +68,141 @@ public class PlayerRunner : MonoBehaviour
         HandleMovement();
     }
 
+    void HandleInput()
+    {
+        Vector2 input = moveAction.ReadValue<Vector2>();
+
+        // Mobile input override
+        if (mobileMoveInput != Vector2.zero)
+            input = mobileMoveInput;
+
+        if (canSwitchLane)
+        {
+            if (input.x > 0.5f && currentLane < 2)
+            {
+                currentLane++;
+                StartCoroutine(LaneSwitchCooldown());
+            }
+            else if (input.x < -0.5f && currentLane > 0)
+            {
+                currentLane--;
+                StartCoroutine(LaneSwitchCooldown());
+            }
+        }
+    }
+
+    IEnumerator LaneSwitchCooldown()
+    {
+        canSwitchLane = false;
+        yield return new WaitForSeconds(0.2f);
+        canSwitchLane = true;
+    }
+
     void HandleMovement()
     {
-
         float targetX = (currentLane - 1) * laneDistance;
         Vector3 currentPosition = transform.position;
-        float newX = Mathf.MoveTowards(currentPosition.x, targetX, laneSwitchSpeed * Time.deltaTime);
+
+        float newX = Mathf.MoveTowards(
+            currentPosition.x,
+            targetX,
+            laneSwitchSpeed * Time.deltaTime
+        );
+
         float xDelta = newX - currentPosition.x;
 
-
+        // Gravity
         velocity.y += gravity * Time.deltaTime;
 
-
         if (velocity.y < 0)
-        {
             velocity.y += gravity * (fallMultiplier - 1) * Time.deltaTime;
-        }
 
-        // Jump
         bool jumpPressed = jumpAction.triggered || mobileJumpInput;
 
         if (controller.isGrounded)
         {
-            if (velocity.y < 0) velocity.y = -2f;
+            if (velocity.y < 0)
+                velocity.y = -2f;
 
             if (jumpPressed && !isFinished)
             {
                 velocity.y = jumpForce;
 
-                if (animator != null) animator.SetTrigger("Jump");
+                if (animator != null)
+                    animator.SetTrigger("Jump");
 
                 mobileJumpInput = false;
             }
         }
 
-        float currentForwardMove = isFinished ? 0f : forwardSpeed * Time.deltaTime;
+        float currentForwardMove = isFinished
+            ? 0f
+            : forwardSpeed * Time.deltaTime;
 
-        Vector3 moveVector = new Vector3(xDelta, velocity.y, currentForwardMove);
+        Vector3 moveVector = new Vector3(
+            xDelta,
+            velocity.y,
+            currentForwardMove
+        );
 
         controller.Move(moveVector);
     }
 
-    void HandleInput()
+    private void OnTriggerEnter(Collider other)
     {
-        float inputSystemX = moveAction.ReadValue<Vector2>().x;
-
-        float horizontalInput = inputSystemX;
-
-        if (Mathf.Abs(mobileMoveInput.x) > Mathf.Abs(horizontalInput))
-            horizontalInput = mobileMoveInput.x;
-
-        if (canSwitchLane)
+        if (other.CompareTag("FinishLine") && !isFinished)
         {
-            if (horizontalInput > 0.5f && currentLane < 2)
-            {
-                currentLane++;
-                canSwitchLane = false;
-            }
-            else if (horizontalInput < -0.5f && currentLane > 0)
-            {
-                currentLane--;
-                canSwitchLane = false;
-            }
+            StartCoroutine(CompleteLevelRoutine());
+        }
+    }
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        if (hit.gameObject.CompareTag("Obstacle") && !isFinished)
+        {
+            TriggerDeath();
+        }
+    }
+
+    void TriggerDeath()
+    {
+        isFinished = true;
+
+        if (animator != null)
+        {
+            animator.SetTrigger("Die");
+            animator.SetBool("IsRunning", false);
         }
 
-        if (Mathf.Abs(horizontalInput) < 0.1f)
-            canSwitchLane = true;
+        velocity = Vector3.zero;
+
+        Debug.Log("Game Over!");
+    }
+
+    IEnumerator CompleteLevelRoutine()
+    {
+        isFinished = true;
+
+        if (animator != null)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+
+        yield return new WaitForSeconds(uiDelay);
+
+        if (levelCompletePanel != null)
+        {
+            levelCompletePanel.SetActive(true);
+        }
     }
 
     void UpdateAnimator()
     {
-        if (animator == null) return;
-
-        animator.SetBool("IsRunning", true);
-        animator.SetBool("IsGrounded", controller.isGrounded);
-        animator.SetFloat("VerticalVelocity", velocity.y);
+        if (animator != null)
+        {
+            animator.SetBool("IsRunning", !isFinished);
+            animator.SetBool("IsGrounded", controller.isGrounded);
+        }
     }
 
     public void MoveInput(Vector2 value)
@@ -147,23 +213,5 @@ public class PlayerRunner : MonoBehaviour
     public void JumpInput(bool value)
     {
         mobileJumpInput = value;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("FinishLine"))
-        {
-            CompleteLevel();
-        }
-    }
-
-    void CompleteLevel()
-    {
-        isFinished = true;
-        if (animator != null)
-        {
-            animator.SetBool("IsRunning", false);
-            animator.SetTrigger("Idle"); 
-        }
     }
 }
